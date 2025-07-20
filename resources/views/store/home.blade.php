fb957ad777a6d665025e9812550982288c9002af
kravza3026 <kravza3026@gmail.com>
2025-07-20 16:04:13 +0300

    Improve newsletter section layout and responsive design
    - Adjusted `md:max-w` class for text container width to enhance readability.
    - Updated background element with absolute positioning and resized for better responsiveness.

--- resources/views/store/home.blade.php
+++ resources/views/store/home.blade.php
@@ -61,7 +61,7 @@
         <section class="pt-section">
             <div class="grid grid-cols-1 md:flex items-center relative justify-start bg-olive  h-fit min-h-[380px] xl:min-h-[560px]">
                 <div class="container order-2 md:order-1 pb-7 md:pb-0 relative h-full grid items-center content-center">
-                    <div class="md:max-w-10/12">
+                    <div class="md:max-w-5/12">
                         <h2 class="section-title text-balance text-white md:text-[24px] xl:text-[40px] xl:leading-12 font-[700] py-5">
                             Subscribe to newsletter and get 25% off your first order
                         </h2>
@@ -76,7 +76,7 @@
                         </form>
                     </div>
                 </div>
-                <div class="w-full md:w-7/12 min-h-[380px] xl:min-h-[560px] order-1 md:order-2"
+                <div class="w-full md:absolute right-0 md:w-6/12 min-h-[380px] xl:min-h-[560px] order-1 md:order-2"
                 style="background-image: url({{ asset('assets/images/subscribe_bg.jpg') }}); background-size: cover; background-position: center; background-repeat: no-repeat;"
                 >
 
