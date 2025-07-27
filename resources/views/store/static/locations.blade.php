5d237b5db84d737ed2de8f7b5c1b2b8f4eaee208
Edward Z <edwardzaim@icloud.com>
2025-07-27 16:57:49 -0400

    Small fixes/improvements, added footer translation, organizing folder structure.

--- resources/views/store/static/locations.blade.php
+++ resources/views/store/static/locations.blade.php
@@ -1,7 +1,21 @@
 <x-app-layout>
+    @push('head')
+        <!-- // Here are the styles -->
+{{--        <link rel="stylesheet" href="{{ asset('css/locations.css') }}">--}}
+    @endpush
+
     <div class="pageContent">
         <section class="container pt-section">
             Locations {{ app()->getLocale() }}
         </section>
     </div>
+
+    @push('scripts')
+        <script>
+            document.addEventListener('DOMContentLoaded', function() {
+                // Initialize any JavaScript functionality here if needed
+                console.log('Locations page loaded');
+            });
+        </script>
+    @endpush
 </x-app-layout>
