dc7a0dad7579aca8fac3c26c5bf9720f8278ebcc
Edward Z <edwardzaim@icloud.com>
2025-08-19 00:41:36 -0500

    Some address improvements.

--- resources/views/store/account/addresses/index.blade.php
+++ resources/views/store/account/addresses/index.blade.php
@@ -3,18 +3,6 @@
         <div class="bg-white lg:shadow sm:rounded-xl">
             @include('store.account.nav')
             <div data-vue-component="Addresses" class="p-4 lg:p-10"></div>
-
-            @unless ($user->shippingAddresses()->count() && $user->billingAddresses()->count())
-{{--            <div class="flex flex-col justify-center items-center my-12 py-12">--}}
-{{--                <div class="flex justify-center items-center -mb-6">--}}
-{{--                    <img src="{{ Vite::image('common/empty_addresses.jpg') }}" alt=""/>--}}
-{{--                </div>--}}
-{{--                <h3 class="flex justify-center items-center font-extrabold text-lg text-gray-900">--}}
-{{--                    --}}{{-- // TODO - Translate.--}}
-{{--                    {{ __('No saved addresses') }}--}}
-{{--                </h3>--}}
-{{--            </div>--}}
-            @endunless
         </div>
     </div>
 </x-app-layout>
